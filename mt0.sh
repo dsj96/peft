50fd7f8b38b1551bf069ad1207b3dc1fdf590355
dsj96 <51779172+dsj96@users.noreply.github.com>
2023-07-20 17:42:30 +0800

    Update mt0.sh

--- mt0.sh
+++ mt0.sh
@@ -1,7 +1,7 @@
 model=mt0-xxl
 config_file=./ds_zero3_config/default_config.yaml
-dataset_name=./data/opus-100/opus-100-corpus/v1.0/opus_train_dev_tst/opus_train_labse_dev_tst_add_task
-raw_dataset_name=opus_train_labse_dev_tst_add_task
+dataset_name=./data/opus-100/opus-100-corpus/v1.0/opus_train_dev_tst/opus9_select_train_labse_dev_tst_add_task
+raw_dataset_name=opus9_select_train_labse_dev_tst_add_task
 r=8
 lora_alpha=32
 lora_dropout=0.1
@@ -21,7 +21,7 @@ echo $output_dir
 accelerate launch --config_file=$config_file examples/mt0_peft_lora_ds_zero3_offload.py --model_name_or_path=$model_name_or_path --dataset_name=$dataset_name\
                     --r=$r --lora_alpha=$lora_alpha --lora_dropout=$lora_dropout --text_column=$text_column --label_column=$label_column \
                     --num_epochs=$num_epochs --batch_size=$batch_size\
-                    --do_validation --save_interval=$save_interval --validation_interval=$validation_interval --validation_ratio=$validation_ratio\
+                    --save_interval=$save_interval --validation_interval=$validation_interval --validation_ratio=$validation_ratio\
                     --src_max_length=$src_max_length --output_dir=$output_dir --max_new_tokens=$max_new_tokens --num_beams=$num_beams \
                     --monitor_config=./ds_zero3_config/monitor_config.json\
                     --deepspeed_config_file=./ds_zero3_config/ds_offload_config.json
